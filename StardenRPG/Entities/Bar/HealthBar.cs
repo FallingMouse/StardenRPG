722f1101966457921847fbe242685579cc8af22b
Kittikan Kongoat <kittikankongaut@gmail.com>
2023-05-28 05:47:28 +0700

    Health bar success

--- StardenRPG/Entities/Bar/HealthBar.cs
+++ StardenRPG/Entities/Bar/HealthBar.cs
@@ -9,15 +9,14 @@ namespace StardenRPG.Entities.Bar
         protected Player _player;
 
         // Variables for the offset of the health bar relative to the player
-        private float xOffset = 0f; 
-        private float yOffset = -12f;
+        private float xOffset = -2.6f; 
+        private float yOffset = -3f;
 
-        private Texture2D _texture;
-        private Vector2 _position;
-        private Rectangle _rectangle;
-        private Rectangle _rectangleBG;
-        private Color _color;
-        private Color _colorBG;
+        private Texture2D _texture, _healthBox;
+        private Vector2 _position, _positionBox;
+        private Rectangle _rectangle, _rectangleBG, _rectangleBox;
+        private Color _color, _colorBG;
+
 
         public HealthBar(GraphicsDevice graphicsDevice, Player _player)
         {
@@ -25,11 +24,22 @@ namespace StardenRPG.Entities.Bar
 
             _texture = new Texture2D(graphicsDevice, 1, 1);
             _texture.SetData(new[] { Color.White });
-            _position = new Vector2(7, 12);
+
+            _position = new Vector2(0, 0);
+            _positionBox = new Vector2(0, 0);
+
             _rectangle = new Rectangle((int)_position.X, (int)_position.Y, _player.CharacterStats.MaxHealth, 1);
             _rectangleBG = new Rectangle((int)_position.X, (int)_position.Y, _player.CharacterStats.MaxHealth, 1);
+            _rectangleBox = new Rectangle((int)_position.X, (int)_position.Y, _player.CharacterStats.MaxHealth + 60, 1);
+
             _color = Color.Red;
             _colorBG = Color.Gray;
+            
+        }
+
+        public void SetHealthBox(Texture2D texture2) 
+        {
+            _healthBox = texture2;
         }
 
         public void Update(GameTime gameTime, Player _player)
@@ -38,6 +48,7 @@ namespace StardenRPG.Entities.Bar
 
             // Update the health bar's position based on the player's position
             _position = new Vector2(_player.Position.X + xOffset, _player.Position.Y - yOffset);
+            _positionBox = new Vector2(_player.Position.X + xOffset, _player.Position.Y - yOffset);
 
             _rectangle.Location = _position.ToPoint();
             _rectangleBG.Location = _position.ToPoint();
@@ -45,16 +56,8 @@ namespace StardenRPG.Entities.Bar
 
         /*public void Draw(SpriteBatch spriteBatch, Player player)
         {
-            _rectangleBG.Width = _player.CharacterStats.MaxHealth / 12;
-            _rectangle.Width = _player.CharacterStats.CurrentHealth / 12;
-            spriteBatch.Draw(_texture, _rectangleBG, _colorBG);
-            spriteBatch.Draw(_texture, _rectangle, _color);
-
-        }*/
-        public void Draw(SpriteBatch spriteBatch, Player player)
-        {
             // Define the scale for the health bar
-            Vector2 scale = new Vector2(0.5f, 0.5f);  // Scale the height by 0.5 (half)
+            Vector2 scale = new Vector2(0.9f, 0.5f);  // Scale the height by 0.5 (half)
 
             // Calculate the center of the health bar for rotation
             //Vector2 origin = new Vector2(_rectangle.Width / 2, _rectangle.Height / 2);
@@ -90,8 +93,55 @@ namespace StardenRPG.Entities.Bar
                 effects: SpriteEffects.None,
                 layerDepth: 0
             );
+        }*/
+
+        public void Draw(SpriteBatch spriteBatch, Player player)
+        {
+            // Define the scale for the health bar
+            Vector2 scale = new Vector2(0.9f, 0.5f);  // Scale the height by 0.5 (half)
+
+            _rectangleBG.Width = _player.CharacterStats.MaxHealth / 18;
+            _rectangle.Width = _player.CharacterStats.CurrentHealth / 18;
+
+
+            // Draw the background of the health bar
+            spriteBatch.Draw(
+                texture: _texture,
+                position: _position,
+                sourceRectangle: _rectangleBG,
+                color: _colorBG,
+                rotation: 0,
+                origin: Vector2.Zero,
+                scale: scale,
+                effects: SpriteEffects.None,
+                layerDepth: 0
+            );
+
+            // Draw the actual health bar
+            spriteBatch.Draw(
+                texture: _texture,
+                position: _position,
+                sourceRectangle: _rectangle,
+                color: _color,
+                rotation: 0,
+                origin: Vector2.Zero,
+                scale: scale,
+                effects: SpriteEffects.None,
+                layerDepth: 0
+            );
+
+            // Draw the health box
+            spriteBatch.Draw(
+                texture: _healthBox,
+                position: _positionBox,
+                sourceRectangle: _rectangleBox,
+                color: Color.White,
+                rotation: 0,
+                origin: Vector2.Zero,
+                scale: scale,
+                effects: SpriteEffects.None,
+                layerDepth: 0
+            );
         }
-
-
     }
 }
--- StardenRPG/Screens/TestScreen.cs
+++ StardenRPG/Screens/TestScreen.cs
@@ -72,6 +72,7 @@ namespace StardenRPG.Screens
 
         // Health Bar
         private HealthBar _healthBar;
+        private Texture2D _healthBox;
 
         // Add the input state object
         private InputState input = new InputState();
@@ -136,8 +137,12 @@ namespace StardenRPG.Screens
             Camera.EnableTracking = true;
             #endregion
 
+            #region Load Content
             // Health Bar
             _healthBar = new HealthBar(ScreenManager.Game.GraphicsDevice, _player);
+            _healthBox = ScreenManager.Content.Load<Texture2D>("Backgrounds/Bar/HealthBox");
+            _healthBar.SetHealthBox(_healthBox);
+            #endregion
 
             // once the load has finished, we use ResetElapsedTime to tell the game's
             // timing mechanism that we have just finished a very long frame, and that
